47bb863d8870bca6e9e143b5c0713aeb9106be1f
rob <you@example.com>
2023-01-02 17:08:01 -0500

    Started optlib class definition

---
+++ optlib.py
@@ -0,0 +1,32 @@
+## Analog CMOS Optimization Library
+# Author: Robert Block
+#
+# License Information
+# FREEWARE LICENSE AGREEMENT
+# Derived from the Analog CMOS Design, Tradeoffs, and Optimization
+# design spreadsheet. Copyright 2000-2008 David M. Binkley
+
+"""
+Class optlib
+Analog Optimization Library
+
+Usage:
+Initialize library with CMOs Process Parameters
+Use calculation functions to derive desired results
+"""
+class optlib:
+
+    """
+    Optlib initialization
+    """
+    def __init__(self):
+        # Call import scripts here for model parameters
+        None
+
+    """
+    Function IdFromVeff
+    Inputs: Veff, Vsb, W, L
+    Output: Id (uA)
+    """
+    def IdFromVeff(Veff,Vsb,W,L):
+        None
